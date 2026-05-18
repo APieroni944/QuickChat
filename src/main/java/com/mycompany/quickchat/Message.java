775fa7214ce854662fae102bb50a46133faa720b
APieroni944 <st10479944@myemeris.edu.za>
2026-05-18 15:53:26 +0200

    Restructured main method and added Part2 method

--- src/main/java/com/mycompany/quickchat/Message.java
+++ src/main/java/com/mycompany/quickchat/Message.java
@@ -18,7 +18,8 @@ public class Message {
   String RecipientCell;
   String Message;
   String Hash;
-
+  
+  }
   public Message(int numSent, String recipientCell, String message) {
     Random rand = new Random();
     ID = rand.nextInt(1000000000, 10000000000);
@@ -37,7 +38,7 @@ public class Message {
     String[] splitMessage = this.Message.split(" ").toUpperCase;
     return this.ID.substring(0, 2) + ":" + Integer.toString(this.NumSent) + ":" + splitMessage[0] + splitMessage[splitMessage.length()-1];
   }
-  public void SentMessage() {
+  public Message SentMessage() {
     String option;
     Scanner scanner = new scanner
     system.out.println("What do you want to do with the message? press the number fot the option you want: \n     0: Disregard message \n     1: Send Message \n     2: Save message to send later \n(default = 1)");
@@ -45,16 +46,35 @@ public class Message {
     switch (option) {
       case "0": {
         //Disregard message
-        break;
+        System.out.println("Disregarding message")
+        return 0;
       };
       case "2": {
         //save message
-        break;
+        System.out.println("Saving message")
+        return 2;
       };
       default: {
         //send message
-        break;
+        System.out.println("Message successfully sent");
+        this.printMessageInfo();
+        return 1;
       };
     }
   }
+  public void printMessages(Message[] messages) {
+    for (int i = 0; i < messages.length; i++){
+      system.out.println("-------  Message " + i + "  -------");
+      messages[i].printMessageInfo();
+      system.out.println("-----------------------------------");
+    }
+  }
+  public int returnTotalMessages() {
+    return this.NumSent;
+  }
+  public void printMessageInfo() {
+    system.out.println("Message ID:   " + this.ID + "\nMessage Hash:    " + this.Hash + "\nRecipient:    " + this.RecipientCell + "\nMessage:    " + this.Message)
+  }
 }
+
+
--- src/main/java/com/mycompany/quickchat/QuickChat.java
+++ src/main/java/com/mycompany/quickchat/QuickChat.java
@@ -4,13 +4,46 @@
 
 package com.mycompany.quickchat;
 import java.util.*;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 /**
  *
  * @author lab_services_student
  */
 public class QuickChat {
+    Message[] sent = new Message[100];
+    int sentIndex = 0;
+    Message[] saved = new Message[100];
+    int savedindex;
+    Message[] deleted = new Message[100];
+    int deletedindex;
+    Scanner scanner = new Scanner(System.in);       //Create instance of scanner
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);       //Create instance of scanner
+      User user = Part1();
+    }
+
+    public Message[] RetrieveFile(String path) {
+        try {
+            String jsonString = Files.writeString(Paths.get(path));
+            return gson.fromJson(jsonString, Message[].class);
+        } catch (IOException e) {
+            System.err.println("Failed to read file in path " + path + ": " + e.getMessage());
+            return null;
+        }
+    }
+    public void SaveFile(Message[] messages, String path){
+        try {
+            String jsonString = gson.toJson(messages);
+            Files.writeString(Paths.get(path), jsonString);
+        } catch (IOException e) {
+            System.err.println("Failed to write file in path " + path + ": " + e.getMessage());
+        }
+    }
+
+    public user Part1() {
         Login login = new Login();                      //create instance of login
         User user = null;                               //initialise current user as null
         String UName;
@@ -44,5 +77,41 @@ public class QuickChat {
             }
         }
         System.out.println("current user: " + user.getUserName());      //print username of current user
+        return user;
+    }
+    public void Part2(User user) {
+        try {
+            SavedMessages = RetrieveFile("SavedMessages.json");
+            for(; SavedIndex < PastMessages.length(); SavedIndex++) {
+                Saved[SavedIndex] = SavedMessages[SavedIndex];
+            }
+            SentIndex -= 1;
+        } catch (Exception e) {
+            system.out.println("Failed to retrieve past messages: " + e.getMessage())
+        }
+
+
+        System.out.println("Welcome to QuickChat \n");
+        while(true) {
+            System.out.println("Select an option: \n  1: Send messages \n  2: Show recently sent messages \n  3: Quit \n(defautl = 1)");
+            
+            String option = scanner.nextLine();
+            switch (option) {
+                case "2": {
+                    System.out.println("Coming soon")
+                }
+                case "3": {
+                    //Quit program
+                    try {
+                        SaveFile(SavedMessages, "SavesMessages.json")
+                    } catch (Exception e) {
+                        System.out.println("Failed to save past messages: " + e.getMessage())
+                    }
+                    System.exit(0);
+                }
+                default: {}
+            }
+        }
     }
 }
+
